2bb714030e8ecbf560cb3d99bab6a392a757c315
Ariel <arielbanua76@gmail.com>
2024-07-08 20:02:16 +0800

    tried sentence similarity model

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        if(request.form['answer'] == ''):
+            return "<html><body> <h1>Invalid answer</h1></body></html>"
+        else:
+            answer = request.form['answer']
+            print('test')
+            sentences = ["this is a very good sentence", answer]
+
+            model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+            embeddings = model.encode(sentences)
+            result = cosine_similarity([embeddings[0]], embeddings[1:])
+
+            return render_template('answer.html', answer=answer, result=result)
+    if request.method == 'GET':
+        return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
